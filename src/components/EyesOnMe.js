ca11825e73d9507dad15a69a1d7fce05e85fae85
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-04-21 23:23:11 +0300

    completed labs

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe() {
+  const focusHandler = () => {
+    console.log("Good!");
+  };
+  const blurHandler = () => {
+    console.log("Hey! Eyes on Me");
+  };
+  return (
+    <div>
+      <button onFocus={focusHandler} onBlur={blurHandler}>
+        Eyes On Me
+      </button>
+    </div>
+  );
+}
